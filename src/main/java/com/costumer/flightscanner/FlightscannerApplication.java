58c05195ef56a53173f77d6750eea4ade5040ac7
SlartiBartFast-art <JekHamond@tutanota.com>
2022-05-12 21:11:28 +0500

    Initial commit

---
+++ src/main/java/com/costumer/flightscanner/FlightscannerApplication.java
@@ -0,0 +1,13 @@
+package com.costumer.flightscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightscannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightscannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/costumer/flightscanner/FlightscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costumer.flightscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightscannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
